104d48b18a052dbde968f19f56cbf06814687577
jonaspmeise <62212517+jonaspmeise@users.noreply.github.com>
2022-11-13 13:10:02 +0100

    Added automatical render+download of SVG-canvas

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ function uploadFiles(event) {
 
 function updateListSelection(selectContext, selectedIndex) {
 
-    let selectedElement = selectContext[selectedIndex]
+    let selectedElement = selectContext[selectedIndex];
 
     updateElementByExtension(selectedElement.filePath.split('.').pop())(selectedElement);
 }
@@ -86,6 +86,10 @@ function excelFileToJSON(file){
         });
 
         let sheetnames = workbook.SheetNames;
+        let worksheetTabs = document.getElementById('worksheetTabs');
+
+        //delete all current tabs and create a new tab-button for each worksheet
+        removeAllChildNodes(worksheetTabs);
 
         sheetnames.forEach(sheetname => {
             let switchButton = document.createElement('button');
@@ -94,7 +98,7 @@ function excelFileToJSON(file){
             switchButton.classList.add('tablinks');
             switchButton.addEventListener('click', function() {renderTable(this.name)});
 
-            document.getElementById('worksheetTabs').appendChild(switchButton);
+            worksheetTabs.appendChild(switchButton);
         });
 
         database = Object.fromEntries(
@@ -102,6 +106,12 @@ function excelFileToJSON(file){
                 sheetname => [sheetname, XLSX.utils.sheet_to_json(workbook.Sheets[sheetname])]));
         renderTable('Karten');
     };
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
 
 //Method to display the data in HTML Table
@@ -124,4 +134,48 @@ function renderTable(sheetname){
    table.on("rowClick", function(e, row){ 
         console.log(row.getData());
    });
+}
+
+
+//for delayed updating of the SVG image
+let triggerSVGChange = debounce(renderSVG, 500);
+
+function renderSVG(sourceCode) {
+    const myblob = new Blob([sourceCode], {
+        type: 'image/svg+xml'
+    });
+
+    saveSvgAsImage(URL.createObjectURL(myblob));
+}
+
+function debounce(func, timeout = 300){
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+}
+
+function saveSvgAsImage(imageUrl) {
+    const canvas = document.getElementById('svgCanvas');
+    const ctx = canvas.getContext('2d');
+
+    const img = new Image();
+    img.onload = function() {
+        canvas.width = this.width;
+        canvas.height = this.height;
+        ctx.drawImage(this, 0, 0);
+
+        const downloadableImage = canvas.toDataURL('image/png');
+        console.log(downloadableImage);
+        var aDownloadLink = document.createElement('a');
+        // Add the name of the file to the link
+        aDownloadLink.download = 'canvas_image.png';
+        // Attach the data to the link
+        aDownloadLink.href = downloadableImage;
+        // Get the code to click the download link
+        aDownloadLink.click();
+    }
+
+    img.src = imageUrl;
 }
